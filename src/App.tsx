b12cfde49b33b99af0ebc46da5ef1de8bcb11dd2
meti <mahdi.mazloomi03@gmail.com>
2023-06-14 16:11:06 +0400

    starting login/signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/*" element={<Navigate to="./signup"></Navigate>}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Login.module.css"
+const Login = () => {
+    return <div></div>
+}
+
+export default Login;
