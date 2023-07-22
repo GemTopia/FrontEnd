c6fd99230b247a5a24054d622bc0459d39c70661
NiknazHabiba <niknazhabiba82@gmail.com>
2023-07-23 03:37:33 +0430

    Responsive Game Result Page

--- src/pages/GameResult.tsx
+++ src/pages/GameResult.tsx
@@ -3,7 +3,7 @@ import style from "./GameResult.module.css";
 
 const GameResult = () => {
 
-  const [isWinner , setIsWinner] = useState<boolean>(true); 
+  const [isWinner , setIsWinner] = useState<boolean>(false); 
 
 
   return (
@@ -30,15 +30,16 @@ const GameResult = () => {
           className={style["winning-gif"]}
         />
       </div>}
-      <div className={style.body}>
-        {!isWinner ? <p className={style.score}>Score : 2,788</p> :
+      {!isWinner ? <p className={style.score}>Score : 2,788</p> :
         <p className={style.score}>Best Score : 2,788</p>
         }
-        <p className={style.rank}>Rank :  143</p>
+      <div className={style.body}>
+        <p className={style.rank}>Rank :&nbsp;&nbsp;&nbsp;&nbsp;143</p>
         <div className={style.token}>
           <img src={require("../assets/rotated-gemyto.png")} alt="gemyto" className={style.gemyto} />
           <p className={style["token-count"]}>12</p>
         </div>
+        <p className={style["earning-condition"]}>You will earn Gemyto by the end of the day if you stay among the top players .</p>
         <button className={style["play-button"]}> Play Again</button>
         <div className={style["footer-buttons-container"]}>
           <span className={style["footer-button"]}>
